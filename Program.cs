641d21e2b86a81754f56532a16544f716be82c12
Blueyeskon <konstancia_milova@hotmail.com>
2024-10-11 20:41:29 -0400

    new test

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp4;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        Console.WriteLine("Hello, This program will reserve a string.");
+        Console.WriteLine(("Please enter a word:"));
+
+        //convert to lower case to check for palindrome
+        string word = (Console.ReadLine().ToLower());
+
+        Console.WriteLine($"You have entered: {word}");
+
+
+    }
+}
